fb51d9e92f697bd7d9c44b8f37e9cb08f331a3d1
spudunk <hicksme@gmail.com>
2022-08-24 11:43:08 -0700

    update favicon

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,6 +21,25 @@ export default function Home() {
         <title>Fernweh Outfitters</title>
         <meta name="description" content="Homepage of Fernweh Outfitters" />
         <link rel="icon" href="/favicon.ico" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className={styles.main}>
